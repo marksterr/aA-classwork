8d5082fd93b64ae05d7e2a0402bfb4cb58fa2278
Grayson Poon <grayson.a.poon@gmail.com>
2021-09-23 14:29:47 -0700

    Describe #two_sum RSpec tests

--- week_4/day_4/first_tdd/lib/tdd.rb
+++ week_4/day_4/first_tdd/lib/tdd.rb
@@ -1,3 +1,22 @@
 def my_uniq(arr)
-  [1,2,3,4,5]
+  result = []
+  arr.each { |el| result << el if !result.include?(el) }
+  return result
+end
+
+class Array
+
+  def two_sum
+    return nil if self == []
+    
+    result = []
+    (0..self.size - 2).each do |i|
+      (i + 1..self.size - 1).each do |j|
+        result << [i, j] if self[i] + self[j] == 0
+      end
+    end
+
+    return result
+  end
+
 end
--- week_4/day_4/first_tdd/spec/tdd_spec.rb
+++ week_4/day_4/first_tdd/spec/tdd_spec.rb
@@ -9,3 +9,19 @@ describe "my_uniq" do
     expect(my_uniq([1,2,2,3,4,5])).to eq([1,2,3,4,5]) 
   end
 end
+
+describe "Array" do
+
+  describe "#two_sum" do
+    context "when array is empty" do
+      it "should return nil" do
+        expect([].two_sum).to eq(nil)
+      end
+    end
+
+    it "should return a new array with pairs of positions that add to zero" do
+      expect([-1, 0, 2, -2, 1].two_sum).to eq([[0,4], [2,3]])
+    end
+  end
+
+end
