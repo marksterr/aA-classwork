eb9ba4adefaa146febb784ed389a80905a25fd7c
kw-8 <kwu11235813@gmail.com>
2021-09-30 17:51:30 -0700

    Course and enrollment associations complete

--- week_5/day_4/skeleton/app/models/course.rb
+++ week_5/day_4/skeleton/app/models/course.rb
@@ -11,6 +11,13 @@
 #
 class Course < ApplicationRecord
 
-  
+  has_many :enrollments,
+    class_name: 'Enrollment',
+    primary_key: :id,
+    foreign_key: :course_id
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :user
 
 end
