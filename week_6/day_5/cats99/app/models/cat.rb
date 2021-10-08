69d99019c0fd6a9bb6ce086d2cc75460a1330315
Mark <mrkdawes8@gmail.com>
2021-10-08 18:43:49 -0400

    add cat index

--- week_6/day_5/cats99/app/models/cat.rb
+++ week_6/day_5/cats99/app/models/cat.rb
@@ -2,16 +2,16 @@ class Cat < ApplicationRecord
 
   validates :birth_date, :name, presence: true
   validates :name, uniqueness: true
-  validate :gender
+  #validate :gender
   
-  def age
+  # def age
     
-  end
+  # end
 
-  def gender
-    if :sex != 'M' || :sex != 'F'
-      errors.add(:sex, 'Invalid')
-    end
-  end
+  # def gender
+  #   if :sex != 'M' && :sex != 'F'
+  #     errors.add(:sex, 'Invalid')
+  #   end
+  # end
 
 end
