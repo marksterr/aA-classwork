6192e67c4e90175905c6473d0a248e1abf37d4e0
Mark <mrkdawes8@gmail.com>
2021-10-11 18:17:58 -0400

    add sessioncontroller

--- week_7/day_1/skeleton/app/controllers/application_controller.rb
+++ week_7/day_1/skeleton/app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+
+  helper_method: current_user
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def sign_in(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def sign_out
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def require_signed_in
+    redirect_to new_session_url unless signed_in?
+  end
+
 end
--- week_7/day_1/skeleton/app/controllers/sessions_controller.rb
+++ week_7/day_1/skeleton/app/controllers/sessions_controller.rb
@@ -1,3 +1,21 @@
 class SessionsController < ApplicationController
   
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user.save
+      sign_in(@user)
+      redirect_to users_url
+    else
+      render json: ['Invalid Username or Password']
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
 end
